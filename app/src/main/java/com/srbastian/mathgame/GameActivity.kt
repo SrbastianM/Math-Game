26f3c59fb8d5710e466a63c6ee2d66504188586f
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-02-18 15:00:26 -0500

    cr/BL: Start of the app's logic conformation

--- app/src/main/java/com/srbastian/mathgame/GameActivity.kt
+++ app/src/main/java/com/srbastian/mathgame/GameActivity.kt
@@ -7,10 +7,22 @@ import android.widget.EditText
 import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
+    lateinit var textLife : TextView
+    lateinit var textScore : TextView
+    lateinit var textTime : TextView
+    lateinit var textQuestion : TextView
 
+    lateinit var editTextAnswer : EditText
+
+    lateinit var buttonCheck : Button
+    lateinit var buttonNext : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-
+        textLife = findViewById(R.id.tv_score_reached)
+        textLife = findViewById(R.id.tv_lives_remaining)
+        textLife = findViewById(R.id.tv_time_left)
+        buttonCheck = findViewById(R.id.btn_check)
+        buttonNext = findViewById(R.id.btn_next)
     }
 }
