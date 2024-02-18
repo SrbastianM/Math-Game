d9d97396bdf889723bc2f377dbd6197044ed97ab
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-02-18 01:44:55 -0500

    cr/UI: I did changes in fonts beacuse isn't work as me expect

--- app/src/main/java/com/srbastian/mathgame/GameActivity.kt
+++ app/src/main/java/com/srbastian/mathgame/GameActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class GameActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game2)
