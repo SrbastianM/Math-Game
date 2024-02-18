cc0a69da27e96173f1444f7fa942c042bf5c3012
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-02-18 14:41:45 -0500

    cr/UI: I did the plus quiz activity (named like game activity)

--- app/src/main/java/com/srbastian/mathgame/GameActivity.kt
+++ app/src/main/java/com/srbastian/mathgame/GameActivity.kt
@@ -2,11 +2,15 @@ package com.srbastian.mathgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game2)
+        setContentView(R.layout.activity_game)
+
     }
 }
